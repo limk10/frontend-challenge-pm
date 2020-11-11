460d699dd92f059501e13c0219056a8a0fea7ed1
Matheus Lopes <matheus_barbosa5@hotmail.com>
2020-11-11 00:29:39 -0300

    Ajustes de tamanho de input e mensagens de validação

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -5,9 +5,7 @@ export const validate = async (schema, form) => {
   let errors = "";
 
   await schema.validate(form, { abortEarly: false }).catch(({ inner }) => {
-    inner.map(({ path, message }) => {
-      errors += "-" + message + "\n";
-    });
+    inner.map(({ path, message }) => (errors += "-" + message + "\n"));
   });
 
   return errors;
@@ -15,16 +13,21 @@ export const validate = async (schema, form) => {
 
 export const transaction = yup.object().shape({
   customerName: yup.string().required("O campo nome da pessoa é obrigatório!"),
-  document: yup.string().required("O campo CPF é obrigatório!"),
-  cardNumber: yup.string().required("O campo n° do cartão é obrtaório!"),
+  document: yup
+    .string()
+    .max(11, "O CPF deve conter 11 digitos!")
+    .required("O campo CPF é obrigatório!"),
+  cardNumber: yup
+    .string()
+    .min(16, "O campo n° do cartão deve conter 16 digitos!")
+    .required("O campo n° do cartão é obrtaório!"),
   expirationDate: yup
     .string()
+    .min(4, "A expiração deve ser no formato MM/YY!")
     .required("O campo data expiração é obrigatório!"),
-  cvv: yup.string().required("O campo cvv é obrigatório!"),
+  cvv: yup
+    .string()
+    .min(4, "O cvv deve conter no mínimo 6 digitos!")
+    .required("O campo cvv é obrigatório!"),
   amount: yup.number().required("O campo valor transação é obrigatório!")
-});
-
-export const client = yup.object().shape({
-  firstName: yup.string().required("O primeiro nome é obrigatório!"),
-  lastName: yup.string().required("O último nome é obrigatório!")
 });
