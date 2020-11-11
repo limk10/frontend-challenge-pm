315323999b620b8107731259e80a6ce4b998336e
Matheus Lopes <matheus_barbosa5@hotmail.com>
2020-11-11 00:27:41 -0300

    Ajustado pagina não encontrada para redirecionar usuario

--- src/screens/NotFound/index.js
+++ src/screens/NotFound/index.js
@@ -1,13 +1,34 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { Typography } from "@material-ui/core";
 import { Container } from "./styles";
+import { useHistory } from "react-router-dom";
 
-function NotFound() {
+const NotFound = () => {
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(() => {
+      history.push("/");
+    }, 4000);
+  }, []);
+
   return (
     <Container>
-      <h1>Pagina não encontrada</h1>
+      <Typography
+        style={{ textAlign: "center", paddingTop: 50 }}
+        variant="h5"
+        gutterBottom
+      >
+        Pagina não encontrada :(
+      </Typography>
+      <Typography
+        style={{ textAlign: "center", paddingTop: 50 }}
+        variant="h6"
+        gutterBottom
+      >
+        Você será redirecionado para pagina principal em alguns segundos!
+      </Typography>
     </Container>
   );
-}
+};
 
 export default NotFound;
